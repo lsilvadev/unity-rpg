f2b65475adb8ae15c7162e8301ee9683d1442eb0
Leticia Silva <leticia.silva@acesso.io>
2018-06-26 20:01:33 -0300

    Transport behaviour

---
+++ Assets/Game/Script/Util/TransportBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportBehaviour : MonoBehaviour {
+
+    private Camera _camera;
+    public Transform point;
+
+	void Start () 
+    {
+        _camera = Camera.main;
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (point==null)
+        {
+            Debug.Log("TransportBehaviour - Point Error");
+        }
+        else
+        {
+            col.transform.position = point.transform.position;
+            _camera.gameObject.transform.position = point.transform.position + new Vector3(0, 0, -10);
+        }
+
+
+    }
+	
+}
