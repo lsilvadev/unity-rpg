701df3948aa9d4f2b777fca85e8602d7c358026d
Leticia Silva <leticia.silva@acesso.io>
2018-06-27 12:30:13 -0300

    Player killing, enemy movement and attacking enemies

--- Assets/Game/Script/EnemyController.cs
+++ Assets/Game/Script/EnemyController.cs
@@ -1,50 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour {
 
-    public float moveSpeed;
-    private Rigidbody2D rigibody;
+    public float moveSpeed; //velocidade do inimigo  
     private bool moving;
+    private Rigidbody2D rigibody;
 
-    public float timeBetweenMove;
-    private float timeBetweenMoveCounter;
-    public float timeToMove;
-    private float timeToMoveCounter;
+    public float timeBetweenMove; //tempo para mudar de posição
+    private float timeBetweenMoveCounter; //tempo atual de mudança de posição
+    public float timeToMove; //tempo de movimentação
+    private float timeToMoveCounter; //tempo atual de movimentação
 
-    private Vector3 moveDirection;
+    private Vector3 moveDirection; //direção
+    public float waitToReload;
+    private bool reloading;
+    private GameObject player;
 
 	void Start () {
         rigibody = GetComponent<Rigidbody2D>();
 
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 	}
 	
 	void Update () {
-		if (moving)
+        Move();
+
+        if(reloading)
+        {
+            waitToReload -= Time.deltaTime;
+
+            if (waitToReload < 0)
+            {
+                //Application.LoadLevel(Application.loadedLevel); //obsolete
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                player.SetActive(true);
+            }
+        }
+	}
+
+    void Move()
+    {
+        if (moving)
         {
             timeToMoveCounter -= Time.deltaTime;
             rigibody.velocity = moveDirection;
 
-            if (timeToMoveCounter<0f)
+            if (timeToMoveCounter < 0f)
             {
                 moving = false;
-                timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
             }
         }
         else
         {
             timeBetweenMoveCounter -= Time.deltaTime;
             rigibody.velocity = Vector2.zero;
+
             if (timeBetweenMoveCounter < 0f)
             {
                 moving = true;
-                timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
-                moveDirection = new Vector3(Random.Range(-1f, 1f)*moveSpeed, Random.Range(-1f, 1f)*moveSpeed, 0f);
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
             }
         }
-	}
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.name=="Player")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            reloading = true;
+            player = other.gameObject;
+        }
+    }
 }
---
+++ Assets/Game/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private bool isWalk;
+    private Vector2 stop;
+    private Animator animator;
+
+	void Start () 
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () 
+    {
+        Moviment();
+	}
+
+    void Moviment()
+    {                
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        isWalk = false;
+
+        if (horizontal!=0)
+        {
+            transform.Translate(horizontal * Time.deltaTime * speed, 0, 0);
+            isWalk = true;
+            stop = new Vector2(horizontal, 0);
+        }
+        if (vertical!=0)
+        {
+            transform.Translate(0, vertical * Time.deltaTime * speed, 0);
+            isWalk = true;
+            stop = new Vector2(0, vertical);
+        }
+
+        animator.SetFloat("PositionY", vertical);
+        animator.SetFloat("PositionX", horizontal);        
+        animator.SetBool("Walk", isWalk);
+        animator.SetFloat("StopX", stop.x);
+        animator.SetFloat("StopY", stop.y);
+    }
+}
